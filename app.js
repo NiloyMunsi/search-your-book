781e73b20beda9ae5ad0f8c4ceb36fbc7e8c2e1a
NiloyMunsi <niloymunsi717@gmail.com>
2021-09-02 16:19:15 +0600

    running

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ searchBtn.addEventListener("click", function () {
       
       <div class="col">
       <div class="card h-100">
-      <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="...">
+      <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
         <div class="card-body">
         <h5 class="card-title">Book Name: ${book.title}</h5>
         <p class="card-text">Author Name: ${book.author_name}</p>
